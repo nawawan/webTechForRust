27b7e9547d8e94b834dae7deb687eb8afdb2e4e4
nawawan <mizutake0428@yahoo.co.jp>
2025-03-09 22:25:35 +0900

    feat: implement routing

--- src/main.rs
+++ src/main.rs
@@ -23,26 +23,37 @@ fn handle_connection(mut stream: TcpStream, items: &mut Vec<String>) {
     let contents = req.split(" ").map(|s| s.to_string()).collect::<Vec<String>>();
 
 
-    match &contents[0] as &str {
-        "POST" => {
+    match (&contents[0] as &str , &contents[1] as &str ){
+        ("POST", "/add") => {
             println!("POST");
             items.push(contents.last().unwrap().split("=").last().unwrap().to_string());
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n {}", response_html(items));
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
         },
-        "GET" => {
+        ("GET", "/todo") => {
             println!("GET");
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n {}", response_html(items));
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        },
+        ("GET", "/favicon.ico") => {
+            println!("Not Found: favicon.ico");
+        },
+        ("GET", "/static/todo.css") => {
+            println!("Not Found: /static/todo.css");
         },
         _ => {
             println!("Invalid request");
         }
     }
 
+}
+
+fn response_html(items: &mut Vec<String>) -> String {
     let mut context = Context::new();
     context.insert("todos", &items);
     let tera: Tera = Tera::new("templates/**/*").unwrap();
     let rendered: String = tera.render("todo.html", &context).unwrap();
-
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n {}", rendered);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    rendered
 }
